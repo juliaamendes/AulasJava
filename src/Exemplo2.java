81c9991482b10aff1d5d3e03facf4cdcc4c238df
Julia Mendes <cordeiroj647@gmail.com>
2024-10-04 10:31:26 -0300

    Operadores Aritimeticos dia 04/10/24

---
+++ src/Exemplo2.java
@@ -0,0 +1,20 @@
+
+public class Exemplo2 {
+    public static void main(String[]args){
+        int total, num1=2, num2=4;
+
+        total = num1 + num2;
+        System.out.println(" O total da soma é " +total);
+        total = num1 - num2;
+        System.out.println(" O total da subtração é " +total);
+        total = num1 * num2;
+        System.out.println("O total da multiplicação é " +total);
+
+        float totalFloat, numFloat1 = 2, numFloat2 =4;
+        totalFloat = numFloat1 / numFloat2;
+        System.out.println("O total da divisão é " +totalFloat);
+
+
+        }
+
+}
