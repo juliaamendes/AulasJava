3777e5ebee5d17647dbc53f4c48d12c5bc03f717
Julia Mendes <cordeiroj647@gmail.com>
2024-10-04 11:01:47 -0300

    Operadores de comparação dia 04/10/24

--- src/Exemplo3.java
+++ src/Exemplo3.java
@@ -1,2 +1,27 @@
 public class Exemplo3 {
+    public static void main(String[] args) {
+        int num1= 3, num2= 7;
+        boolean resposta;
+
+        resposta = num1 > num2;
+        System.out.println(num1 + ">" +num2 + "=" + resposta);
+
+        resposta = num1 > num2;
+        System.out.println(num1 + "<" + num2 + "=" + resposta);
+
+        resposta = num1 > num2;
+        System.out.println(num1 + ">=" + num2 + "=" + resposta);
+
+        resposta = num1 > num2;
+        System.out.println(num1 + "<=" + num2 + "=" + resposta);
+
+        resposta = num1 > num2;
+        System.out.println(num1 + "==" + num2 + "=" + resposta);
+
+        resposta = num1 > num2;
+        System.out.println(num1 + "!=" + num2 + "=" + resposta);
+
+
+    }
+
 }
