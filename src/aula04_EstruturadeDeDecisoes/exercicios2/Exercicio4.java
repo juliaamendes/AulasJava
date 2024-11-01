f21e9c636d5954211a997a3969e1fd8dc36e709d
Julia Mendes <cordeiroj647@gmail.com>
2024-11-01 11:09:06 -0300

    dia 1/11

---
+++ src/aula04_EstruturadeDeDecisoes/exercicios2/Exercicio4.java
@@ -0,0 +1,16 @@
+package aula04_EstruturadeDeDecisoes.exercicios2;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o valor de vendas");
+        double vendas = scanner.nextDouble();
+        double comissao;
+
+        if (vendas< 1000){
+
+
+    }
+}
---
+++ src/aula05/Exemplo05.java
@@ -0,0 +1,12 @@
+package aula05;
+
+public class Exemplo05 {
+    public static void main(String[] args) {
+        for (int i = 0; i< 10; i++){
+            if (i==5){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
