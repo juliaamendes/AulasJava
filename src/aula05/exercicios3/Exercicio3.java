cb39189bce561a1d5a0e23566f6ad87ba52dae7b
Julia Mendes <cordeiroj647@gmail.com>
2024-11-17 11:09:52 -0300

    Create Exercicio3.java

---
+++ src/aula05/exercicios3/Exercicio3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class Exercicio3 {
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+ 
+        System.out.print("Digite um numero inteiro positivo: ");
+        int numero = scanner.nextInt();
+        int fatorial = 1;
+        int i = numero;
+ 
+        do {
+            fatorial * = i;
+            i--;
+        } while (i > 0);
+ 
+        System.out.println("O fatorial de " + numero + " é: " + fatorial);
+        scanner.close();
+    }
+}
