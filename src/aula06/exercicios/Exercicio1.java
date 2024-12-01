5a240a31a6cac0d2544738154621e6ec7793a9c0
Julia Mendes <cordeiroj647@gmail.com>
2024-12-01 20:23:53 -0300

    exercícios

--- src/aula06/exercicios/Exercicio1.java
+++ src/aula06/exercicios/Exercicio1.java
@@ -1,13 +1,25 @@
 package aula06.exercicios;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercicio1 {
     public static void main(String[] args) {
-     Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite um numero");
-        int numero = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int[] numero = new int[5];
+        int soma = 0;
+
+        
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Digite um numero: ");
+            numero[i] = scanner.nextInt();
+            soma += numero[i]; 
+        }
+
+       
+        System.out.println("A soma dos numeros é: " + soma);
+        scanner.close();
+    }
+}
 
 
     }
